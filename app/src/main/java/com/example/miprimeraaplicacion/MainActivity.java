5e36c7e8bc521abeafdbccae9f8fa851bc91c32c
samu603 <samuelantoniog75@gmail.com>
2025-02-14 23:16:16 -0600

    Creacion y funcionamiento de la rama sensor de proximidad

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -6,10 +6,9 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,47 +24,68 @@ public class MainActivity extends AppCompatActivity {
 
         sensorLuz();
     }
+
     @Override
     protected void onResume() {
-        iniciar();
         super.onResume();
+        iniciar();
     }
+
     @Override
     protected void onPause() {
-        detener();
         super.onPause();
+        detener();
     }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
-    }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-    private void sensorLuz(){
-        tempVal = findViewById(R.id.lblSensorLuz);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if( sensor==null ){
-            tempVal.setText("Tu dispositivo, NO tiene el senor de LUZ");
-            finish();
-        }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                double valor = event.values[0];
-                tempVal.setText("Cantidad de Luz: "+ valor);
-                if(valor<=20){
-                    getWindow().getDecorView().setBackgroundColor(Color.GRAY);
-                }else if(valor<=50){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                }else{
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                }
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            }
-        };
+
+    private void iniciar() {
+        if (sensor != null) {
+
+            sensorManager.registerListener(sensorEventListener, sensor, 2000 * 1000);
         }
     }
 
+    private void detener() {
+        if (sensor != null) {
+
+            sensorManager.unregisterListener(sensorEventListener);
+        }
+    }
+
+    private void sensorLuz() {
+        tempVal = findViewById(R.id.lblSensorProximidad);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (sensor == null) {
+            tempVal.setText("Tu dispositivo NO tiene el sensor de proximidad");
+            finish();
+        }
+
+
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+
+                if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                    double valor = event.values[0];
+
+                    tempVal.setText("Proximidad: " + valor);
+
+
+                    if (valor <= 4) {
+                        getWindow().getDecorView().setBackgroundColor(Color.BLACK);
+                    } else if (valor <= 8) {
+                        getWindow().getDecorView().setBackgroundColor(Color.GRAY);
+                    } else {
+                        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+}
