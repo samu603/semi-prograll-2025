67de6ac2d36ef385b14b1f5b0c76b89f3fafdd1e
samu603 <samuelantoniog75@gmail.com>
2025-02-14 23:00:21 -0600

    Uso de pestañas (Tabhost) y conversores

--- app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -3,63 +3,71 @@ package com.example.miprimeraaplicacion;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tbh;
     Button btn;
     TextView tempVal;
     Spinner spn;
+    Conversores objConversores = new Conversores();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tbh = findViewById(R.id.tbhConversor);
+        tbh.setup();
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("TIEMPO", null));
+        tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+
+        // Configurar el botón de cálculo
         btn = findViewById(R.id.btnCalcular);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tempVal = findViewById(R.id.txtNum1);
-                double num1 = Double.parseDouble(tempVal.getText().toString());
-                tempVal = findViewById(R.id.txtNum2);
-                double num2 = Double.parseDouble(tempVal.getText().toString());
-                double respuesta = 0.0;
+                // Obtener la pestaña activa
+                int opcion = tbh.getCurrentTab();
 
-                String msg = "";
+                // Obtener el Spinner y la cantidad para la conversión
+                spn = findViewById(R.id.spnDeMonedas);
+                int de = spn.getSelectedItemPosition();
 
-                spn = findViewById(R.id.spnOpciones);
-                switch (spn.getSelectedItemPosition()){
-                    case 0:
-                        respuesta = num1 + num2;
-                        msg = "La suma es: "+ respuesta;
-                        break;
-                    case 1:
-                        respuesta = num1 - num2;
-                        msg = "La resta es: "+ respuesta;
-                        break;
-                    case 2:
-                        respuesta = num1 * num2;
-                        msg = "La multiplicaciom es: "+ respuesta;
-                        break;
-                    case 3:
-                        respuesta = num1 / num2;
-                        msg = "La division  es: "+ respuesta;
-                        break;
+                spn = findViewById(R.id.spnAMonedas);
+                int a = spn.getSelectedItemPosition();
 
-                }
+                tempVal = findViewById(R.id.txtCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                // Realizar la conversión
                 tempVal = findViewById(R.id.lblRespuesta);
-                tempVal.setText("Respuesta: "+ respuesta);
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+                double respuesta = objConversores.convertir(opcion, de, a, cantidad);
+                tempVal.setText("Respuesta: " + respuesta);
             }
         });
+    }
 
+    class Conversores {
+        double[][] valores = {
+                {1, 0.98, 7.73, 25.45, 36.78, 508.87, 8.74}, // monedas
+                {1, 1000, 100, 0.01}, // longitud
+                {1, 60, 3600}, // tiempo (segundos a minutos, horas)
+                {1, 1024, 1048576}, // almacenamiento (bytes a kilobytes, megabytes)
+        };
+
+        // Método para realizar la conversión
+        public double convertir(int opcion, int de, int a, double cantidad) {
+            if (opcion < valores.length && de < valores[opcion].length && a < valores[opcion].length) {
+                return valores[opcion][a] / valores[opcion][de] * cantidad;
+            }
+            return 0;
+        }
     }
 }
-
